51a8287fab1cf688c953cc4ccfb93ea13766544e
HP <fatiqhussnain1@gmail.com>
2023-06-24 02:04:26 -0700

    code for activity life cycle 7 functions

--- ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "onCreate() is called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart() is called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume() is called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onResume() is called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause() is called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy() is called", Toast.LENGTH_LONG).show()
     }
 }

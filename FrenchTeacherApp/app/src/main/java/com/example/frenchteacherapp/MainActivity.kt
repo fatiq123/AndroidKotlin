98727f0914db89c072e57e1524cec1423b24bfe9
HP <fatiqhussnain1@gmail.com>
2023-06-27 00:30:29 -0700

    French Teacher App

--- FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.kt
+++ FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.frenchteacherapp
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sayColorName(view: View){
+
+        val clickedButton: Button = view as Button
+
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(
+            this,
+            resources.getIdentifier(
+                clickedButton.tag.toString(),
+                "raw",
+                packageName
+            )
+        )
+        mediaPlayer.start()
     }
 }

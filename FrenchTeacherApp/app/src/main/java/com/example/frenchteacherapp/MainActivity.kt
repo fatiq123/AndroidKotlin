f2b27209cda7932a86e4738b46e7d4f11c414689
HP <fatiqhussnain1@gmail.com>
2023-06-27 04:59:40 -0700

    French Teacher App

--- FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.kt
+++ FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.kt
@@ -12,18 +12,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun sayColorName(view: View){
+    fun sayColorName(view: View) {
 
         val clickedButton: Button = view as Button
 
-        val mediaPlayer: MediaPlayer = MediaPlayer.create(
-            this,
-            resources.getIdentifier(
-                clickedButton.tag.toString(),
-                "raw",
-                packageName
-            )
-        )
-        mediaPlayer.start()
+//        val mediaPlayer: MediaPlayer = MediaPlayer.create(
+//            this,
+//            resources.getIdentifier(
+//                clickedButton.tag.toString(),
+//                "raw",
+//                packageName
+//            )
+//        )
+//        mediaPlayer.start()       it is not good practice to use getIdentifier instead we should use R... method to allocate resources
+
+
+        val resourceId = when (clickedButton.tag.toString()) {
+            "yellow" -> R.raw.yellow
+            "red" -> R.raw.red
+            "green" -> R.raw.green
+            "purple" -> R.raw.purple
+            "black" -> R.raw.black
+            else -> 0  // Default value in case no match found
+        }
+        if (resourceId != 0) {
+            val mediaPlayer: MediaPlayer = MediaPlayer.create(this, resourceId)
+            mediaPlayer.start()
+        }
     }
 }

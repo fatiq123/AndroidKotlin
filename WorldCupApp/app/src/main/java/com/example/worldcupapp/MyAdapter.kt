e315523956258d726318fcb8a3877088d98655eb
HP <fatiqhussnain1@gmail.com>
2023-06-27 21:45:43 -0700

    World Cup App complete code

--- WorldCupApp/app/src/main/java/com/example/worldcupapp/MyAdapter.kt
+++ WorldCupApp/app/src/main/java/com/example/worldcupapp/MyAdapter.kt
@@ -1,16 +1,22 @@
 package com.example.worldcupapp
 
 import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
-class MyAdapter(private var activity: Activity, private var items: ArrayList<CountryModel>): BaseAdapter() {
+class MyAdapter(private var activity: Activity, private var items: ArrayList<CountryModel>) :
+    BaseAdapter() {
     override fun getCount(): Int {
         return items.size
     }
 
-    override fun getItem(position: Int): Any {
+    override fun getItem(position: Int): CountryModel {
         return items[position]
     }
 
@@ -19,6 +25,48 @@ class MyAdapter(private var activity: Activity, private var items: ArrayList<Cou
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val view: View?
+        val viewHolder: ViewHolder
+
+        if (convertView == null) {
+
+            val inflater =
+                activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+            view = inflater.inflate(R.layout.list_item_layout, null)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+        val countries = items[position]
+        viewHolder.txtName?.text = countries.name
+        viewHolder.txtCupWin?.text = countries.cupWins
+        viewHolder.flagImg?.setImageResource(countries.flagImg)
+
+        view?.setOnClickListener() {
+            Toast.makeText(
+                activity,
+                "You Choose: ${countries.name}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        return view as View
+    }
+
+    private class ViewHolder(row: View?) {
+        var txtName: TextView? = null
+        var txtCupWin: TextView? = null
+        var flagImg: ImageView? = null
+
+        init {
+            this.txtName = row?.findViewById(R.id.tvCountryName)
+            this.txtCupWin = row?.findViewById(R.id.tvWins)
+            this.flagImg = row?.findViewById(R.id.ivCountryImage)
+        }
+
     }
 }

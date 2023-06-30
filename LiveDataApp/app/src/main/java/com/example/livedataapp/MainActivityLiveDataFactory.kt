93dee961e9026c7305fbd1ca2e8f4394982c94df
HP <fatiqhussnain1@gmail.com>
2023-06-30 06:37:53 -0700

    LiveDataApp using ViewModel and LiveData

--- LiveDataApp/app/src/main/java/com/example/livedataapp/MainActivityLiveDataFactory.kt
+++ LiveDataApp/app/src/main/java/com/example/livedataapp/MainActivityLiveDataFactory.kt
@@ -2,6 +2,14 @@ package com.example.livedataapp
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-class MainActivityLiveDataFactory: ViewModel() {
+class MainActivityLiveDataFactory(private val startingNum: Int) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainActivityLiveData::class.java)) {
+            return MainActivityLiveData(startingNum) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
 }

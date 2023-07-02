653dfcf42da79095fdec5362fe75fb658c8a0d53
HP <fatiqhussnain1@gmail.com>
2023-07-02 08:06:15 -0700

    TabLayout using fragments

--- TabLayoutApp/app/src/main/java/com/example/tablayoutapp/adapters/ViewPagerAdapter.kt
+++ TabLayoutApp/app/src/main/java/com/example/tablayoutapp/adapters/ViewPagerAdapter.kt
@@ -8,14 +8,15 @@ import com.example.tablayoutapp.fragments.BooksFragment
 import com.example.tablayoutapp.fragments.GamesFragment
 import com.example.tablayoutapp.fragments.MoviesFragment
 
-class ViewPagerAdapter(fragmentManager: FragmentManager, lifeCycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifeCycle){
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifeCycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifeCycle) {
 
     override fun getItemCount(): Int {
         return 3        // as we know the exact fragments to display that's why i use direct value if more then we will declare a variable
     }
 
     override fun createFragment(position: Int): Fragment {
-        when(position){
+        when (position) {
             0 -> return GamesFragment()
             1 -> return BooksFragment()
             2 -> return MoviesFragment()

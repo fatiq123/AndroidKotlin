07561ed1a1e6089fcbc093a87f94846d6cf161c1
HP <fatiqhussnain1@gmail.com>
2023-07-05 00:53:37 -0700

    working on UpdateNoteFragment.kt

--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.notetakingapp.MainActivity
 import com.example.notetakingapp.R
@@ -55,7 +57,28 @@ class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
 
 
 
-        binding.etUpdateNoteTitle.text
+        binding.etUpdateNoteTitle.setText(currentNote.noteTitle)
+        binding.etUpdateNoteBody.setText(currentNote.noteBody)
+
+
+        // if the user update the note
+        binding.fabDone.setOnClickListener {
+            val title = binding.etUpdateNoteTitle.text.toString().trim()
+            val body = binding.etUpdateNoteBody.text.toString().trim()
+
+            if (title.isEmpty()){
+                val note = Note(currentNote.id,title,body)
+                notesViewModel.updateNote(note = note)  // this will update the note
+                view.findNavController().navigate(R.id.action_updateNoteFragment_to_homeFragment)
+            } else {
+                Toast.makeText(
+                    view.context,
+                    "Please enter note title",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
 
     }
 

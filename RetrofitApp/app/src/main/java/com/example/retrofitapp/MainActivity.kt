190b4553af97ce49e9ebdbcec4e68d10e0274899
HP <fatiqhussnain1@gmail.com>
2023-07-06 04:03:29 -0700

    Retrofit App using JSON and display it on TextView

--- RetrofitApp/app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ RetrofitApp/app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.retrofitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.example.retrofitapp.databinding.ActivityMainBinding
+import retrofit2.Response
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val retrofitService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Albums>> =
+            liveData {
+                val response = retrofitService.getAlbums()
+                emit(response)
+            }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumItem = albumsList.next()
+//                    Log.i("Tag", albumItem.title)
+
+                    val result = " Album title: ${albumItem.title} \n"
+
+                    binding.tvView.append(result)
+                }
+            }
+        })
+
     }
 }

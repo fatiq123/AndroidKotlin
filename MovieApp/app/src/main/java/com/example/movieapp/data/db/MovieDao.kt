02be82ec87029183c85ad86a55e7eda90d40dc8e
HP <fatiqhussnain1@gmail.com>
2023-07-09 05:48:11 -0700

    MovieDao.kt added with required function

---
+++ MovieApp/app/src/main/java/com/example/movieapp/data/db/MovieDao.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.movieapp.data.Movie
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovies(movies: List<Movie>)
+
+    @Query("DELETE FROM popular_movies")
+    suspend fun deleteAllMovies()
+
+    @Query("SELECT * FROM popular_movies")
+    suspend fun getAllMovies(): List<Movie>
+
+}

591066b9b526b181f3495fe1a071fee96c5be185
HP <fatiqhussnain1@gmail.com>
2023-07-09 05:38:06 -0700

    Retrofit and Api classes added including TMDBService.kt interface

--- MovieApp/app/src/main/java/com/example/movieapp/data/Movie.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/data/Movie.kt
@@ -1,7 +1,9 @@
 package com.example.movieapp.data
 
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "popular_movies")
 data class Movie(
 
     @SerializedName("id")
---
+++ MovieApp/app/src/main/java/com/example/movieapp/data/api/TMDBService.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp.data.api
+
+import com.example.movieapp.data.MovieList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("/movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String)  : Response<MovieList>
+
+}

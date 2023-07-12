7be1d62748b85c950ae96e8427e86c76365e9915
HP <fatiqhussnain1@gmail.com>
2023-07-12 04:05:02 -0700

    Read and Write data using data classes

--- FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.firebaseapp
 
-import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -20,16 +19,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView: TextView = findViewById(R.id.textView)
+        val textView2: TextView = findViewById(R.id.textView2)
 
         // real time database reference
         database = Firebase.database.reference
+
+
+
 
         // Write data to firebase
         database.child("Price").setValue(100)
 
 
-
-        // Read data to firebase
+        // Read data from firebase
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
@@ -43,5 +45,33 @@ class MainActivity : AppCompatActivity() {
         }
         // very important line to read data from firebase
         database.child("Price").addValueEventListener(postListener)
+
+
+
+
+
+
+
+
+        // Writing custom objects to firebase
+
+        val user1 = User("fatiq","123")
+
+        database.child("Users").setValue(user1)
+
+
+        // Reading custom objects from firebase
+        val readFromFirebase = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val post = snapshot.getValue<User>()
+                textView2.text = post.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        }
+
+        database.child("Users").addValueEventListener(readFromFirebase)
     }
 }
---
+++ FirebaseApp/app/src/main/java/com/example/firebaseapp/User.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseapp
+
+data class User(
+    val userName: String = "",
+    val password: String = ""
+)

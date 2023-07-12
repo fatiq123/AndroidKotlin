ae92bfcd41f7c1ac325381f9eee4a6e6e82e5a43
HP <fatiqhussnain1@gmail.com>
2023-07-12 07:53:02 -0700

    Realtime Firebase Database  & Firebase Firestore Database all done from setting and getting data from firebase and documents and collection are also done

--- FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.firebaseapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,18 +25,19 @@ class MainActivity : AppCompatActivity() {
 
         val textView: TextView = findViewById(R.id.textView)
         val textView2: TextView = findViewById(R.id.textView2)
+        val textView3: TextView = findViewById(R.id.textView3)
 
         // real time database reference
         database = Firebase.database.reference
 
+        /* Realtime database starts from here */
 
 
-
-        // Write data to firebase
+        // ---------------------------------------Write data to firebase-----------------------------
         database.child("Price").setValue(100)
 
 
-        // Read data from firebase
+        //----------------------------------------Read data from firebase-----------------------------
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
@@ -47,20 +53,14 @@ class MainActivity : AppCompatActivity() {
         database.child("Price").addValueEventListener(postListener)
 
 
+        //--------------------------------- Writing custom objects to firebase------------------------
+
+        val user = User("fatiq", "123")
+
+        database.child("Users").setValue(user)
 
 
-
-
-
-
-        // Writing custom objects to firebase
-
-        val user1 = User("fatiq","123")
-
-        database.child("Users").setValue(user1)
-
-
-        // Reading custom objects from firebase
+        //--------------------------------Reading custom objects from firebase-----------------------
         val readFromFirebase = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val post = snapshot.getValue<User>()
@@ -73,5 +73,87 @@ class MainActivity : AppCompatActivity() {
         }
 
         database.child("Users").addValueEventListener(readFromFirebase)
+
+
+
+
+
+
+
+        /* Firestore database starts from here */
+
+
+        // Firestore instance
+        val db = Firebase.firestore
+
+        // ---------------------------------Add data-------------------------------------------------
+
+        // Creating a collection
+        val usersCollection = db.collection("User")
+
+        // Create a new user with a first and last name
+        val user1 = hashMapOf(
+            "first" to "Ada",
+            "second" to "Lovelace",
+            "born" to 1815
+        )
+        // Create a new user with a first, middle, and last name
+        val user2 = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912,
+        )
+
+        // Add a new document with a generated ID
+        db.collection("User")
+            .add(user1)
+        db.collection("User")
+            .add(user2)
+
+        // Adding Documents to Collections
+        // db.document("user1").set(user1)   // this is a more line and doing the same thing as above line do
+        //db.document("user2").set(user2)
+
+
+        // ------------------------------------Read data--------------------------------------------
+
+        db.collection("User")
+            .get()
+            .addOnSuccessListener { result ->
+
+//                if (result != null) {
+//                    textView3.text = "${result.metadata}"
+//                }
+                var allDocuments: String = ""   // to get all documents from collection
+                for (document in result) {
+                    allDocuments += "${document.data} "+" \n"
+                    allDocuments += "\n"
+                }
+                textView3.text = allDocuments
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "Unknown exception",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+
+
+
+
+
+
+        //-------------------------------Update Data to Firestore Database---------------------------
+
+        db.collection("User").document().update("first", "fatiq")
+        // i am not applying this change as i don't want to change their names
+
+
+        //-------------------------------Delete Data to Firestore Database---------------------------
+
+        db.collection("User").document().delete()
     }
 }

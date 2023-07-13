777a1789b4ef5fbd5878860967123b9c29a7431b
HP <fatiqhussnain1@gmail.com>
2023-07-12 20:51:05 -0700

    Dependencies Added to Project

--- JournalApp/app/src/main/java/com/example/journalapp/MainActivity.kt
+++ JournalApp/app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.journalapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.journalapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
 }
